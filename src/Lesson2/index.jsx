8b749494d2c70d08c498475d473ecfa58fe0fc06
FozilAzimov <foziljon.azimov@yandex.ru>
2023-04-08 00:01:41 +0500

    props and components

---
+++ src/Lesson2/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class Student extends React.Component {
+  render() {
+    const children = this.props.obj;
+    const child = this.props.child;
+
+
+    // const status = this.props.status;
+    return (
+      <div>
+        {/* <h1>Hey { children?.title } { children?.status }</h1> */ }
+        { child.id } { child.name }
+      </div>
+    )
+  }
+}
+
+
+// let name = "Azimov";
+// class Mentor extends React.Component {
+//   render() {
+//     const firstName = this.props.name;
+//     console.log(firstName);
+//     console.log(this.props);
+//     return (
+//       <div>
+//         <h1>{ name } { firstName }</h1>
+//       </div>
+//     )
+//   }
+// }
+// export { Mentor };
+export default Student;
---
+++ src/Lesson2/review.jsx
@@ -0,0 +1,14 @@
+// 2-dars. Prop va Component
+
+
+// Componentga ta'rif;
+// ReactJS da componentlar asosan bir marta ishlatiladi. Lekin ko'p marta componentlar yaratsak ham bizga hatolik bermaydi.
+// Componentlarning vazifasi biz js da function larni nima maqsadlarda ishlatgan bo'lsak ReactJS da componentlarning vazifasi ham huddi shunday.
+// ReactJS da ' class componentlari va function componentlari ' mavjud. Componentlar nomi har doim katta harf bilan boshlanishi shatr. React hooks bu judaham muhim o'rin tutadi.
+// Componentlarda ham import, export, export default tushinchalari mavjud. export default js ga o'xshab bitta file da faqat bitta bo'ladi.
+// Agar component nomlari bir hil bo'ladigan bo'lsa ularni import qilganda ' as ' kalit so'zi yordamida boshqa yangi nomga almashtirib ketishimiz mumkin.
+
+
+// Propga ta'rif;
+// Componentlarda ' this ' kalit so'zi aynan manashu componentning o'ziga teng boladi. Agar biz render bo'layotgan code ichidagi componentning nomini ichida prop berib yuboriladi.
+// Keyin component ichida render ichida va return dan oldin propni this orqali tutib olishimiz mumkin.

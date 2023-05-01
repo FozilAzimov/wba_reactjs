bffe056c3efb7084a56010b814a061c411ffadfa
FozilAzimov <foziljon.azimov@yandex.ru>
2023-05-01 13:03:14 +0500

    restfull API | fetch | endpoint - url - api - link - silka | GET - PUT - POST - DELETE | new URL()

---
+++ src/Lesson13/components/Fetch/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Fetch() {
+
+  const [name, setName] = useState('Namangan');
+
+  const [data, setData] = useState({});
+
+  const onSelect = (param) => {
+    setName(param);
+  }
+
+  const Endpoint = `http://api.weatherapi.com/v1/current.json?key=ec8bf3f960c94073b35170212233004&q=${ name }&aqi=no`;
+
+  useEffect(() => {
+    fetch(Endpoint, {
+      // method: 'GET','PUT','DELETE','POST'
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json', // shunday yoshilishi shart;
+      },
+      body: JSON.stringify({ id: 7, name: "Gulbashakar" }),
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res.location));
+  }, [name]);
+
+  const url = new URL(`http://api.weatherapi.com/v1/current.json?key=ec8bf3f960c94073b35170212233004&q=${ name }&aqi=no`)
+  console.log(url.searchParams);
+  console.log(url.searchParams.get('q'));
+
+  return (
+    <>
+      {
+        data ?
+          (<div>
+            <h1>Body</h1>
+            <h1>Country: { data.country }</h1>
+            <h1>Localetime: { data.localtime }</h1>
+            <h1>Name: { data.name }</h1>
+          </div>) :
+          (<h1>Loading...</h1>)
+      }
+      <select onChange={ ({ target }) => onSelect(target.value) }>
+        <option value="Namangan">Namangan</option>
+        <option value="Andijon">Andijon</option>
+        <option value="Buxoro">Buxoro</option>
+        <option value="Toshkent">Toshkent</option>
+        <option value="Chust">Chust</option>
+      </select>
+    </>
+  )
+}
---
+++ src/Lesson13/review.js
@@ -0,0 +1,10 @@
+// fetch() - bu Backend dan ma'lumot olib keladi va 2 ta parametr qabul qiladi;
+
+// 1-parametr: url, endpoint, api, link, silka  -->   deb ataladi;
+
+// 2-parametri: Option {} deyiladi. Option ichida 'method' keldi va bu method ning valuesi sifatida ( 'GET', 'PUT' yoki 'Path', 'DELETE' ) beriladi;
+// methodning default qiymati : 'GET' hisoblandi;
+
+
+// url o'zgartirilganda ya'ni filter qilingan url ni olish uchun ' new URL() ' mavjud;
+// new URL dan bizga turli xil methodlar qaytadi : misol uchun ( searchParams, va hokazo );
